d8e57e9d61d4206595b8455559661c5e0e42c103
Eduardo Marques <eduardomar1703@gmail.com>
2026-04-11 16:08:35 -0300

    feat: Implement conection with database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,10 +3,33 @@ package main
 import (
 	"net/http"
 
+	"github.com/EduardoMark/BillingCore/internal/infra/database"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, _ := zap.NewDevelopment()
+	zap.ReplaceGlobals(logger)
+	defer logger.Sync()
+
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		logger.Fatal("Error loading .env file", zap.Error(err))
+	}
+
+	// Initialize database connection
+	_, err := database.New()
+	if err != nil {
+		logger.Fatal("Error connecting to database", zap.Error(err))
+	}
+
+	if err := database.Migrate(); err != nil {
+		logger.Fatal("Error on migrate database", zap.Error(err))
+	}
+
+	// Initialize Gin router
 	router := gin.Default()
 
 	router.GET("/", func(ctx *gin.Context) {
---
+++ internal/infra/database/db.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func New() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Sao_Paulo",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("Error on connect with db: %w", err)
+	}
+
+	DB = db
+
+	return DB, nil
+}
+
+func Migrate() error {
+	return DB.AutoMigrate()
+}
